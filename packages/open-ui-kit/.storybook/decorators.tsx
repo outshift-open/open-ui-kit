4b13c18796a191a147524af80a878a37f5b5ba48
Paul Sterpu <psterpu@cisco.com>
2025-09-22 08:06:03 +0300

    fix: persist dark theme between pages
    Signed-off-by: Paul Sterpu <psterpu@cisco.com>

--- packages/open-ui-kit/.storybook/decorators.tsx
+++ packages/open-ui-kit/.storybook/decorators.tsx
@@ -1,25 +1,14 @@
 import "./css/typography.css";
 
-import { useEffect } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { darkTheme } from "../src/theme/dark/dark-theme";
 import { lightTheme } from "../src/theme/light/light-theme";
 import { Decorator } from "@storybook/react";
 import React from "react";
-
-import { addons } from "@storybook/preview-api";
-import { DARK_MODE_EVENT_NAME } from "storybook-dark-mode";
-
-const channel = addons.getChannel();
+import { useDarkMode } from "storybook-dark-mode";
 
 const ThemeDecorator: Decorator = (Story) => {
-  const [isDark, setDark] = React.useState(false);
-
-  useEffect(() => {
-    // listen to DARK_MODE event
-    channel.on(DARK_MODE_EVENT_NAME, setDark);
-    return () => channel.off(DARK_MODE_EVENT_NAME, setDark);
-  }, [channel, setDark]);
+  const isDark = useDarkMode();
 
   return (
     <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
