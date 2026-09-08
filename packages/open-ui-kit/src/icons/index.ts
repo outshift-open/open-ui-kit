c0fb82c82d6230cb84c5c24da68896e1f2fec08b
misi-bp <mkaloczy@cisco.com>
2026-09-08 10:18:49 +0200

    feat(icons): add the first Figma-migrated icon set under icons/
    Signed-off-by: misi-bp <mkaloczy@cisco.com>

---
+++ packages/open-ui-kit/src/icons/index.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2025 Cisco Systems, Inc. and its affiliates
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export * from "./ui/agentic-services/agent";
+export * from "./ui/arrows/arrow-down";
+export * from "./ui/arrows/close-fullscreen";
+export * from "./ui/arrows/collapse";
+export * from "./ui/arrows/expand";
+export * from "./ui/arrows/keyboard-arrow-down";
+export * from "./ui/check/check-circle";
+export * from "./ui/check/verified";
+export * from "./ui/close/close";
+export * from "./ui/content/documentation";
+export * from "./ui/content/github";
+export * from "./ui/content/send";
+export * from "./ui/edit-install/delete";
+export * from "./ui/finance/payments";
+export * from "./ui/profile/dark-mode";
+export * from "./ui/profile/light-mode";
+export * from "./ui/profile/person-outline";
+export * from "./ui/question/help";
+export * from "./ui/visibility/lock";
+export * from "./ui/visibility/lock-off";
+export * from "./ui/visibility/visibility";
---
+++ packages/open-ui-kit/src/icons/ui/content/send.tsx
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2025 Cisco Systems, Inc. and its affiliates
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { SvgIcon, SvgIconProps } from "@mui/material";
+
+export function Send(props: SvgIconProps) {
+  return (
+    <SvgIcon {...props} viewBox="0 0 18 15.5095">
+      <path
+        d="M17.3544 8.7103L1.4462 15.4248C1.10187 15.5625 0.774749 15.5324 0.464849 15.3344C0.15495 15.1364 0 14.848 0 14.4693V1.04028C0 0.661516 0.15495 0.373138 0.464849 0.175146C0.774749 -0.0228452 1.10187 -0.0529743 1.4462 0.0847589L17.3544 6.79925C17.7848 6.98863 18 7.30714 18 7.75477C18 8.20241 17.7848 8.52091 17.3544 8.7103ZM2.066 12.9198L14.307 7.75477L2.066 2.58978V6.20528L8.26399 7.75477L2.066 9.30427V12.9198Z"
+        fill="currentColor"
+      />
+    </SvgIcon>
+  );
+}
